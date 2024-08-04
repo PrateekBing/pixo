3e45b4266f29b66e6317a66c9ff1a100027d2a5b
“Prateek <prateek.deshmukh19@gmail.com>
2024-08-04 16:10:30 +0530

    add header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,79 +4,27 @@
 </script>
 
 <style>
-h1 {
-    text-align: center;
-    color: black;
+.bebas-neue-regular {
+  font-family: "Bebas Neue", sans-serif;
+  font-weight: 400;
+  font-style: normal;
 }
 h1 {
-    font-family: Playwrite CO;
-    font-size: 45px;
-    margin-top: 20px;
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 60px;
 }
 </style>
 
 
-<h1>Pixo</h1>
-
-
-
-<!-- <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
-    </div>
+<div class="h-30 flex flex-row items-center justify-center bg-black px-4 text-center">
+    <h1 class="text-white mt-10 mb-10">pixo</h1>
 </div>
 
 
-
-<div class="grid grid-cols-2 md:grid-cols-4 gap-4 m-10">
-    {#each Array.from({ length: Math.ceil(images.length / 3) }) as _, colIndex}
-        <div class="grid gap-4">
-            {#each images.slice(colIndex * 3, colIndex * 3 + 3) as image}
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src={images[0].link}>
-                </div>
-            {/each}
-        </div>
-    {/each}
-</div> -->
-
-<div class="flex flex-row flex-wrap items-center justify-center min-h-screen gap-4">
+<div class="flex flex-row flex-wrap items-center justify-center flex-1 gap-7 mt-7">
     {#each images.slice().reverse() as image, index}
         <div class="my-2">
-            <img alt="e" class="h-auto max-w-full rounded-lg" src={images[images.length - 1 - index].link}>
+            <img alt="e" class="h-auto max-w-96 rounded-lg" src={images[images.length - 1 - index].link}>
         </div>
     {/each}
 </div>
